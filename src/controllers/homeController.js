c21d6e4ce72000272d8c93b053ac4cea783bd763
nakkim <kdkeiie8@gmail.com>
2022-05-31 13:47:24 +0900

    homeController 임시 데이터 삭제

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,3 @@
-let loggedInUser = {
-	name: "nakkim",
-	nickname: "asdf",
-	email: "kdkeiie8@gmail.com"
-}
-
 export const showHome = (req, res) => {
 	return res.render('home', { pageTitle: 'Home' });
 };
