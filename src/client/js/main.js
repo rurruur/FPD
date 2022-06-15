e645dea2bbb931bfabd5c03636201887f95eafc7
nakkim <kdkeiie8@gmail.com>
2022-06-15 16:07:57 +0900

    날짜 포맷 맞춰서 출력
    올라온지 하루 안이면 '~(분/시간) 전'으로 출력

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,26 @@
 import "../scss/styles.scss";
+
+const dateArr = document.querySelectorAll('.createdAt');
+
+const formatDate = date => {
+	const currentTime = Date.now();
+	const createdTime = date.getTime();
+	if (createdTime + 86400000 <= currentTime) {
+		const isoDate = date.toISOString().substring(0, 10);
+		const hour = date.getHours();
+		const minute = date.getMinutes();
+		return `${isoDate} ${hour}:${minute}`;
+	} else {
+		// 1분 60000, 1시간 3600000 하루 86400000
+		const diff = currentTime - createdTime;
+		const minute = parseInt(diff / 60000);
+		const hour = parseInt(diff / 3600000);
+		return hour ? `${hour}시간 전` : `${minute}분 전`;
+	}
+};
+
+if (dateArr) {
+	for (const date of dateArr) {
+		date.textContent = formatDate(new Date(date.textContent));
+	}
+}
